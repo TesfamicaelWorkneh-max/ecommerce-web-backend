5aed0925574c19e756f2b21dc75fe9629c45a686
Tesfamicael <worknehtesfamicael707@gmail.com>
2025-12-25 01:50:08 -0800

    back

--- Backend/socket.js
+++ Backend/socket.js
@@ -76,7 +76,7 @@ export const initSocket = (server) => {
 
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://ecommerce-web-backend-u4em.vercel.app",
       methods: ["GET", "POST"],
       credentials: true,
     },
